3f24957604a39259f50aa71999d83677b4406c4e
AbhirupGhosh-git <ghoshabhirup507@gmail.com>
2023-07-12 22:14:35 +0530

    Initial App Component built

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <a href="upi://pay?pa=upiaddress@okhdfcbank&pn=JohnDoe&cu=INR">Buy Now</a>
+    </React.Fragment>
+  );
+};
+
+export default App;
