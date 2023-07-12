69786493e9264df3d2663116ae9f79a402940fbc
AbhirupGhosh-git <ghoshabhirup507@gmail.com>
2023-07-12 22:44:15 +0530

    Few UPI Resources added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,17 @@ import React from "react";
 const App = () => {
   return (
     <React.Fragment>
-      <a href="upi://pay?pa=upiaddress@okhdfcbank&pn=JohnDoe&cu=INR">Buy Now</a>
+      <a href="upi://pay?pa=upiaddress@okhdfcbank&pn=JohnDoe&cu=INR">
+        Any UPI App
+      </a>
+      <a href="gpay://pay?pa=upiaddress@okhdfcbank&pn=JohnDoe&cu=INR">Gpay</a>
+      <a href="phonepe://pay?pa=upiaddress@okhdfcbank&pn=JohnDoe&cu=INR">
+        PhonePe
+      </a>
+      <a href="paytm://pay?pa=upiaddress@okhdfcbank&pn=JohnDoe&cu=INR">PayTm</a>
+      <a href="amazonpay://pay?pa=upiaddress@okhdfcbank&pn=JohnDoe&cu=INR">
+        Amazon Pay
+      </a>
     </React.Fragment>
   );
 };
