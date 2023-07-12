f8af1261cdd1384b2a83dd6ef88837ef18ee855b
AbhirupGhosh-git <ghoshabhirup507@gmail.com>
2023-07-12 22:49:27 +0530

    Break tags added

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,15 @@ const App = () => {
       <a href="upi://pay?pa=upiaddress@okhdfcbank&pn=JohnDoe&cu=INR">
         Any UPI App
       </a>
+      <br />
       <a href="gpay://pay?pa=upiaddress@okhdfcbank&pn=JohnDoe&cu=INR">Gpay</a>
+      <br />
       <a href="phonepe://pay?pa=upiaddress@okhdfcbank&pn=JohnDoe&cu=INR">
         PhonePe
       </a>
+      <br />
       <a href="paytm://pay?pa=upiaddress@okhdfcbank&pn=JohnDoe&cu=INR">PayTm</a>
+      <br />
       <a href="amazonpay://pay?pa=upiaddress@okhdfcbank&pn=JohnDoe&cu=INR">
         Amazon Pay
       </a>
