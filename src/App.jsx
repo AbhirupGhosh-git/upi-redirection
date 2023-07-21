601d9885d1add41da64c89300111feaaaff1d4ca
AbhirupGhosh-git <ghoshabhirup507@gmail.com>
2023-07-21 12:29:44 +0530

    Mode removed

--- src/App.jsx
+++ src/App.jsx
@@ -19,8 +19,8 @@ const App = () => {
 
   return (
     <React.Fragment>
-      <a href="upi://pay?pa=9163608399@ybl&pn=Abhirup Ghosh&cu=INR&am=1.00&tn=Testing UPI Payment&mode=04">
-        Any UPI App
+      <a href="upi://pay?pa=9163608399@ybl&pn=Abhirup Ghosh&cu=INR&am=1.00&tn=Testing UPI Payment">
+        Any UPI App 2
       </a>
       {/* <br />
       <a href="tez://upi/pay?pa=upiaddress@okhdfcbank&pn=JohnDoe&cu=INR">
